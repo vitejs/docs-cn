1ec3e21f95c47d7a72dc2a00de61591b7f1f6ffc
QC-L <github@liqichang.com>
2021-04-02 10:14:29 +0800

    chore: lint script

--- .vitepress/markdown-it-custom-anchor/index.js
+++ .vitepress/markdown-it-custom-anchor/index.js
@@ -1,4 +1,4 @@
-const anchorMatch = /^.+(\s*\{#([a-z0-9\-_]+?)\}\s*)$/
+const anchorMatch = /^.+(\s*\{#([a-z0-9\-_]+?)\}\s*)$/;
 
 const removeAnchorFromTitle = (oldTitle) => {
   const match = anchorMatch.exec(oldTitle);
@@ -8,18 +8,18 @@ const removeAnchorFromTitle = (oldTitle) => {
 module.exports = function plugin(md) {
   const oldTitle = md.renderer.rules.text;
   md.renderer.rules.text = (tokens, idx, options, env, slf) => {
-    let titleAndId = oldTitle(tokens, idx, options, env, slf);
-    return removeAnchorFromTitle(titleAndId)
+    const titleAndId = oldTitle(tokens, idx, options, env, slf);
+    return removeAnchorFromTitle(titleAndId);
   };
 
-  const oldHead = md.renderer.rules.heading_open;
+  const oldHeading = md.renderer.rules.heading_open;
   md.renderer.rules.heading_open = (tokens, idx, options, env, slf) => {
-    let head = oldHead(tokens, idx, options, env, slf);
+    const head = oldHeading(tokens, idx, options, env, slf);
     const data = md.__data;
     const headers = data.headers || (data.headers = []);
     headers.forEach(element => {
-      element.title = removeAnchorFromTitle(element.title)
+      element.title = removeAnchorFromTitle(element.title);
     });
-    return head
+    return head;
   }
 };
