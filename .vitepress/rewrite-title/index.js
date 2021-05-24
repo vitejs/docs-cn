335d96ee1c90ee2c2c960b9e52f88ca98fdf7c02
ShenQingchuan <tmyeve@163.com>
2021-05-24 15:47:44 +0800

    chore: implement frontmatter-service for title rewriting

--- .vitepress/rewrite-title/index.js
+++ .vitepress/rewrite-title/index.js
@@ -1,28 +1,20 @@
 const path = require('path')
 const fs = require('fs')
+const matterService = require('../utils/frontmatter-service')
 const workspacePath = path.resolve(__dirname, '..', '..')
-const articleDirs = ['blog', 'config', 'guide', 'plugins']
+
 const h1MdRegExp = /^#\s+(.+)\s+(\{#([\w-]+)\})$/
+/** 在此书写所有文章所在的目录名 */
+const articleDirs = ['blog', 'config', 'guide', 'plugins']
 
 const rewriteMarkdownTitle = (filePath) => {
-  fs.readFile(filePath, (err, fileData) => {
-    if (err) {
-      console.warn(`Reading file: ${filePath} failed !`)
-      return
-    }
+  const matter = matterService.open(filePath)
+  const lines = String(matter.file).split(/\r?\n/)
+  const h1Line = lines.find((line) => h1MdRegExp.test(line))
+  if (!h1Line) return
 
-    const lines = String(fileData).split(/\r?\n/)
-    if (h1MdRegExp.test(lines[0])) {
-      const matched = h1MdRegExp.exec(lines[0])
-      const titleMeta = `---\ntitle: ${matched[1]}\n---\n`
-      fs.writeFile(filePath, titleMeta + fileData, (err) => {
-        if (err) {
-          console.warn(`Writing title meta ${matched[1]} failed !`)
-          return
-        }
-      })
-    }
-  })
+  const title = h1MdRegExp.exec(h1Line)[1]
+  matter.set('title', title).save()
 }
 
 const ergodicDirectory = (dirPath) => {
