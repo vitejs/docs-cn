d3fd909f5f810b76957ee3a9c4a5cff60238500a
ShenQingchuan <tmyeve@163.com>
2021-08-14 18:39:39 +0800

    feat: href directives support (close #225)

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -33,5 +33,14 @@ export default {
           )
         ])
     })
+  },
+  enhanceApp({ app, router, siteData }) {
+    // app is the Vue 3 app instance from `createApp()`. router is VitePress'
+    // custom router. `siteData`` is a `ref`` of current site-level metadata.
+    app.directive('href', {
+      mounted(el, binding) {
+        el.href = binding.value;
+      }
+    })
   }
 }
