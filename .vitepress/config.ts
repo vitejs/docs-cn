13f9ab18c0f6962afe314dc9be07b8a13acf5294
Satcom <raspsatcombot@gmail.com>
2024-11-15 00:59:33 +0100

    feat: enhance VitePress configuration for improved Mermaid integration and theme customization

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -38,245 +38,246 @@ const additionalTitle = ((): string => {
   }
 })()
 
-const config = defineConfig({
-  title: 'Satcom - Docs',
-  description: 'DevSecOps Documentation',
-  lang: 'en-EN',
+export default withMermaid(
+  defineConfig({
+    title: 'Satcom - Docs',
+    description: 'DevSecOps Documentation',
+    lang: 'en-EN',
 
-  head: [
-    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }],
-    [
-      'link',
-      { rel: 'alternate', type: 'application/rss+xml', href: '/blog.rss' }
+    head: [
+      ['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }],
+      [
+        'link',
+        { rel: 'alternate', type: 'application/rss+xml', href: '/blog.rss' }
+      ],
+      ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
+      [
+        'link',
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: 'true'
+        }
+      ],
+      [
+        'link',
+        {
+          rel: 'preload',
+          href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Manrope:wght@600&family=IBM+Plex+Mono:wght@400&display=swap',
+          as: 'style'
+        }
+      ],
+      [
+        'link',
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Manrope:wght@600&family=IBM+Plex+Mono:wght@400&display=swap'
+        }
+      ],
+      ['link', { rel: 'me', href: 'https://m.webtoo.ls/@vite' }],
+      ['meta', { property: 'og:type', content: 'website' }],
+      ['meta', { property: 'og:title', content: ogTitle }],
+      ['meta', { property: 'og:image', content: ogImage }],
+      ['meta', { property: 'og:url', content: ogUrl }],
+      ['meta', { property: 'og:description', content: ogDescription }],
+      ['meta', { property: 'og:site_name', content: 'vitejs' }],
+      ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+      ['meta', { name: 'twitter:site', content: '@vite_js' }],
+      ['meta', { name: 'theme-color', content: '#646cff' }],
+      [
+        'script',
+        {
+          src: 'https://cdn.usefathom.com/script.js',
+          'data-site': 'TPLGJZGR',
+          'data-spa': 'auto',
+          defer: ''
+        }
+      ]
     ],
-    ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
-    [
-      'link',
-      {
-        rel: 'preconnect',
-        href: 'https://fonts.gstatic.com',
-        crossorigin: 'true'
-      }
-    ],
-    [
-      'link',
-      {
-        rel: 'preload',
-        href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Manrope:wght@600&family=IBM+Plex+Mono:wght@400&display=swap',
-        as: 'style'
-      }
-    ],
-    [
-      'link',
-      {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Manrope:wght@600&family=IBM+Plex+Mono:wght@400&display=swap'
-      }
-    ],
-    ['link', { rel: 'me', href: 'https://m.webtoo.ls/@vite' }],
-    ['meta', { property: 'og:type', content: 'website' }],
-    ['meta', { property: 'og:title', content: ogTitle }],
-    ['meta', { property: 'og:image', content: ogImage }],
-    ['meta', { property: 'og:url', content: ogUrl }],
-    ['meta', { property: 'og:description', content: ogDescription }],
-    ['meta', { property: 'og:site_name', content: 'vitejs' }],
-    ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
-    ['meta', { name: 'twitter:site', content: '@vite_js' }],
-    ['meta', { name: 'theme-color', content: '#646cff' }],
-    [
-      'script',
-      {
-        src: 'https://cdn.usefathom.com/script.js',
-        'data-site': 'TPLGJZGR',
-        'data-spa': 'auto',
-        defer: ''
-      }
-    ]
-  ],
 
-  themeConfig: {
-    logo: '/logo.svg',
+    themeConfig: {
+      logo: '/logo.svg',
 
-    editLink: {
-      pattern: 'https://github.com/vitejs/docs-cn/edit/main/:path',
-      text: 'Suggest changes to this page'
-    },
+      editLink: {
+        pattern: 'https://github.com/vitejs/docs-cn/edit/main/:path',
+        text: 'Suggest changes to this page'
+      },
 
-    outline: {
-      label: 'On this page',
-      level: [2, 3]
-    },
+      outline: {
+        label: 'On this page',
+        level: [2, 3]
+      },
 
-    search: {
-      provider: 'algolia',
-      options: {
-        appId: '7H67QR5P0A',
-        apiKey: '208bb9c14574939326032b937431014b',
-        indexName: 'vitejs',
-        searchParameters: {
-          facetFilters: ['tags:cn']
-        },
-        placeholder: 'Search documentation',
-        translations: {
-          button: {
-            buttonText: 'Search',
-            buttonAriaLabel: 'Search'
+      search: {
+        provider: 'algolia',
+        options: {
+          appId: '7H67QR5P0A',
+          apiKey: '208bb9c14574939326032b937431014b',
+          indexName: 'vitejs',
+          searchParameters: {
+            facetFilters: ['tags:cn']
           },
-          modal: {
-            searchBox: {
-              resetButtonTitle: 'Clear the query',
-              resetButtonAriaLabel: 'Clear the query',
-              cancelButtonText: 'Cancel',
-              cancelButtonAriaLabel: 'Cancel'
+          placeholder: 'Search documentation',
+          translations: {
+            button: {
+              buttonText: 'Search',
+              buttonAriaLabel: 'Search'
             },
-            startScreen: {
-              recentSearchesTitle: 'Search history',
-              noRecentSearchesText: 'No search history',
-              saveRecentSearchButtonTitle: 'Save to search history',
-              removeRecentSearchButtonTitle: 'Remove from search history',
-              favoriteSearchesTitle: 'Favorites',
-              removeFavoriteSearchButtonTitle: 'Remove from favorites'
-            },
-            errorScreen: {
-              titleText: 'Unable to fetch results',
-              helpText: 'You might need to check your network connection'
-            },
-            footer: {
-              selectText: 'Select',
-              navigateText: 'Navigate',
-              closeText: 'Close',
-              searchByText: 'Search provider'
-            },
-            noResultsScreen: {
-              noResultsText: 'No relevant results found',
-              suggestedQueryText: 'You can try querying',
-              reportMissingResultsText:
-                'Do you think this query should have results?',
-              reportMissingResultsLinkText: 'Let us know'
+            modal: {
+              searchBox: {
+                resetButtonTitle: 'Clear the query',
+                resetButtonAriaLabel: 'Clear the query',
+                cancelButtonText: 'Cancel',
+                cancelButtonAriaLabel: 'Cancel'
+              },
+              startScreen: {
+                recentSearchesTitle: 'Search history',
+                noRecentSearchesText: 'No search history',
+                saveRecentSearchButtonTitle: 'Save to search history',
+                removeRecentSearchButtonTitle: 'Remove from search history',
+                favoriteSearchesTitle: 'Favorites',
+                removeFavoriteSearchButtonTitle: 'Remove from favorites'
+              },
+              errorScreen: {
+                titleText: 'Unable to fetch results',
+                helpText: 'You might need to check your network connection'
+              },
+              footer: {
+                selectText: 'Select',
+                navigateText: 'Navigate',
+                closeText: 'Close',
+                searchByText: 'Search provider'
+              },
+              noResultsScreen: {
+                noResultsText: 'No relevant results found',
+                suggestedQueryText: 'You can try querying',
+                reportMissingResultsText:
+                  'Do you think this query should have results?',
+                reportMissingResultsLinkText: 'Let us know'
+              }
             }
           }
         }
-      }
-    },
-
-    docFooter: {
-      prev: 'Previous page',
-      next: 'Next page'
-    },
-
-    footer: {
-      message: `Released under the MIT License. (${commitRef})`,
-      copyright: 'Copyright © 2024-Now - Satcom'
-    },
-
-    nav: [
-      {
-        component: 'ReleaseTag'
       },
-      {
-        text: 'Infrastructure',
-        link: '/Infrastructure/',
-        activeMatch: '/Infrastructure/'
+
+      docFooter: {
+        prev: 'Previous page',
+        next: 'Next page'
       },
-      { text: 'CyberSec', link: '/CyberSec/', activeMatch: '/CyberSec/' },
-      {
-        text: 'Related Links',
-        items: [
+
+      footer: {
+        message: `Released under the MIT License. (${commitRef})`,
+        copyright: 'Copyright © 2024-Now - Satcom'
+      },
+
+      nav: [
+        {
+          component: 'ReleaseTag'
+        },
+        {
+          text: 'Infrastructure',
+          link: '/Infrastructure/',
+          activeMatch: '/Infrastructure/'
+        },
+        { text: 'CyberSec', link: '/CyberSec/', activeMatch: '/CyberSec/' },
+        {
+          text: 'Related Links',
+          items: [
+            {
+              items: [
+                {
+                  text: 'Bluesky',
+                  link: 'https://bsky.app/profile/vite.dev'
+                }
+              ]
+            }
+          ]
+        }
+      ],
+
+      sidebar: {
+        '/Infrastructure/': [
           {
+            text: 'Infrastructure',
             items: [
               {
-                text: 'Bluesky',
-                link: 'https://bsky.app/profile/vite.dev'
+                text: 'Docker',
+                link: '/Infrastructure/Docker'
+              },
+              {
+                text: 'Traefik',
+                link: '/Infrastructure/Traefik'
+              },
+              {
+                text: 'Wireguard',
+                link: '/Infrastructure/Wireguard'
+              },
+              {
+                text: 'Vaultwarden',
+                link: '/Infrastructure/Vaultwarden'
+              }
+            ]
+          }
+        ],
+        '/CyberSec/': [
+          {
+            text: 'CyberSec',
+            items: [
+              {
+                text: 'CrowdSec',
+                link: '/CyberSec/CrowdSec'
               }
             ]
           }
         ]
       }
-    ],
-
-    sidebar: {
-      '/Infrastructure/': [
-        {
-          text: 'Infrastructure',
-          items: [
-            {
-              text: 'Docker',
-              link: '/Infrastructure/Docker'
-            },
-            {
-              text: 'Traefik',
-              link: '/Infrastructure/Traefik'
-            },
-            {
-              text: 'Wireguard',
-              link: '/Infrastructure/Wireguard'
-            },
-            {
-              text: 'Vaultwarden',
-              link: '/Infrastructure/Vaultwarden'
-            }
-          ]
-        }
+    },
+    transformPageData(pageData) {
+      const canonicalUrl = `${ogUrl}/${pageData.relativePath}`
+        .replace(/\/index\.md$/, '/')
+        .replace(/\.md$/, '/')
+      pageData.frontmatter.head ??= []
+      pageData.frontmatter.head.unshift(
+        ['link', { rel: 'canonical', href: canonicalUrl }],
+        ['meta', { property: 'og:title', content: pageData.title }]
+      )
+      return pageData
+    },
+    markdown: {
+      codeTransformers: [transformerTwoslash()],
+      config(md) {
+        md.use(groupIconMdPlugin)
+      }
+    },
+    vite: {
+      plugins: [
+        groupIconVitePlugin({
+          customIcon: {
+            firebase: 'vscode-icons:file-type-firebase',
+            '.gitlab-ci.yml': 'vscode-icons:file-type-gitlab'
+          }
+        })
       ],
-      '/CyberSec/': [
-        {
-          text: 'CyberSec',
-          items: [
-            {
-              text: 'CrowdSec',
-              link: '/CyberSec/CrowdSec'
-            }
-          ]
-        }
-      ]
+      optimizeDeps: {
+        include: [
+          '@shikijs/vitepress-twoslash/client',
+          'gsap',
+          'gsap/dist/ScrollTrigger',
+          'gsap/dist/MotionPathPlugin'
+        ]
+      }
+    },
+    buildEnd,
+    mermaid: {
+      // Mermaid configuration options
+      theme: 'default',
+      darkMode: true,
+      viewport: {
+        width: 800,
+        height: 600
+      }
     }
-  },
-  transformPageData(pageData) {
-    const canonicalUrl = `${ogUrl}/${pageData.relativePath}`
-      .replace(/\/index\.md$/, '/')
-      .replace(/\.md$/, '/')
-    pageData.frontmatter.head ??= []
-    pageData.frontmatter.head.unshift(
-      ['link', { rel: 'canonical', href: canonicalUrl }],
-      ['meta', { property: 'og:title', content: pageData.title }]
-    )
-    return pageData
-  },
-  markdown: {
-    codeTransformers: [transformerTwoslash()],
-    config(md) {
-      md.use(groupIconMdPlugin)
-    }
-  },
-  vite: {
-    plugins: [
-      groupIconVitePlugin({
-        customIcon: {
-          firebase: 'vscode-icons:file-type-firebase',
-          '.gitlab-ci.yml': 'vscode-icons:file-type-gitlab'
-        }
-      })
-    ],
-    optimizeDeps: {
-      include: [
-        '@shikijs/vitepress-twoslash/client',
-        'gsap',
-        'gsap/dist/ScrollTrigger',
-        'gsap/dist/MotionPathPlugin'
-      ]
-    }
-  },
-  buildEnd
-})
+  })
+)
 
-export default withMermaid(config, {
-  mermaid: {
-    theme: 'default', // Theme for light mode
-    darkTheme: 'dark', // Theme for dark mode
-    sequence: {
-      useMaxWidth: false, // Allows diagrams to grow beyond default width
-      diagramMarginX: 50,
-      diagramMarginY: 10,
-    }
-  }
-})
+
