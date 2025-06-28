8fd795e51d33616bb61078fe75459c5c7737c660
thinkasany <480968828@qq.com>
2025-06-28 09:05:13 +0800

    chore: add old version url

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -258,6 +258,10 @@ export default defineConfig({
         text: '历史版本',
         items: [
           {
+            text: 'Vite v6 文档（英文）',
+            link: 'https://v6.vite.dev'
+          },
+          {
             text: 'Vite v5 文档（英文）',
             link: 'https://v5.vite.dev'
           },
