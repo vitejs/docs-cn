26f8439ece21b03db9318a383a3aad02be1536ea
Satcom <47340064+Satcomx00-x00@users.noreply.github.com>
2025-07-07 09:28:13 +0000

    release: test

--- .vitepress/utils/useReleaseTag.ts
+++ .vitepress/utils/useReleaseTag.ts
@@ -4,7 +4,7 @@ export function useReleaseTag() {
   const releaseTag = ref('')
 
   onMounted(async () => {
-    const result = await fetch('https://api.github.com/repos/vitejs/docs-cn/releases/latest')
+    const result = await fetch('https://api.github.com/repos/satcomx00-x00/docs-cn/releases/latest')
     const json = await result.json()
     releaseTag.value = json?.tag_name ?? ''
   })
